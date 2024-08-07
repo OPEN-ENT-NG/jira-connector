d11347c364b27cdd759238ab3689b920e3564b2f
Guillaume Mareschal <guillaume.mareschal@cgi.com>
2024-08-07 16:24:50 +0200

    feat(migration): #MEX-393 Vertx migration

--- src/main/java/fr/openent/jiraconnector/Jira.java
+++ src/main/java/fr/openent/jiraconnector/Jira.java
@@ -4,6 +4,7 @@ import fr.openent.jiraconnector.config.JiraConnectorConfig;
 import fr.openent.jiraconnector.controller.JiraConnectorController;
 import fr.openent.jiraconnector.service.ServiceFactory;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Promise;
 import org.entcore.common.http.BaseServer;
 import org.entcore.common.neo4j.Neo4j;
 
@@ -11,11 +12,13 @@ public class Jira extends BaseServer {
 
 	private ServiceFactory serviceFactory;
 	@Override
-	public void start() throws Exception {
-		super.start();
+	public void start(Promise<Void> startPromise) throws Exception {
+		super.start(startPromise);
 		JiraConnectorConfig jiraConnectorConfig = new JiraConnectorConfig(config);
 		this.serviceFactory = new ServiceFactory(vertx, Neo4j.getInstance(), jiraConnectorConfig);
 		addController(new JiraConnectorController(serviceFactory));
+		startPromise.tryComplete();
+		startPromise.tryFail("[JIRA-CONNECTOR@Jira::start] Failed to start Jira module.");
 		vertx.deployVerticle(SsoJira.class, new DeploymentOptions().setConfig(config));
 	}
 }
