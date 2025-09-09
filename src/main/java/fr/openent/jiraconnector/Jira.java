212af46355dc5920bc0200b689c81218be57a111
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-09-09 18:02:04 +0200

    feat: #RBACK-117 #RBACK-119, kubernetes compatibility

--- src/main/java/fr/openent/jiraconnector/Jira.java
+++ src/main/java/fr/openent/jiraconnector/Jira.java
@@ -4,6 +4,7 @@ import fr.openent.jiraconnector.config.JiraConnectorConfig;
 import fr.openent.jiraconnector.controller.JiraConnectorController;
 import fr.openent.jiraconnector.service.ServiceFactory;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import org.entcore.common.http.BaseServer;
 import org.entcore.common.neo4j.Neo4j;
@@ -11,14 +12,20 @@ import org.entcore.common.neo4j.Neo4j;
 public class Jira extends BaseServer {
 
 	private ServiceFactory serviceFactory;
+
 	@Override
 	public void start(Promise<Void> startPromise) throws Exception {
-		super.start(startPromise);
-		JiraConnectorConfig jiraConnectorConfig = new JiraConnectorConfig(config);
-		this.serviceFactory = new ServiceFactory(vertx, Neo4j.getInstance(), jiraConnectorConfig);
-		addController(new JiraConnectorController(serviceFactory));
-		startPromise.tryComplete();
-		startPromise.tryFail("[JIRA-CONNECTOR@Jira::start] Failed to start Jira module.");
-		vertx.deployVerticle(SsoJira.class, new DeploymentOptions().setConfig(config));
+    final Promise<Void> promise = Promise.promise();
+		super.start(promise);
+    promise.future()
+      .compose(e -> this.init())
+      .onComplete(startPromise);
 	}
+
+  private Future<Void> init() {
+    JiraConnectorConfig jiraConnectorConfig = new JiraConnectorConfig(config);
+    this.serviceFactory = new ServiceFactory(vertx, Neo4j.getInstance(), jiraConnectorConfig);
+    addController(new JiraConnectorController(serviceFactory));
+    return vertx.deployVerticle(SsoJira.class, new DeploymentOptions().setConfig(config)).mapEmpty();
+  }
 }
