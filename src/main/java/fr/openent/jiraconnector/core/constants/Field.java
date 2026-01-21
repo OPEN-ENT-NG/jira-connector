b094407d51e7188ec27de2916393795b4f82c2f9
moustaphahennawi <56674781+moustaphahennawi@users.noreply.github.com>
2026-01-21 19:43:04 +0100

    feat: CLC-9 add Jira SCIM Provisioning

--- src/main/java/fr/openent/jiraconnector/core/constants/Field.java
+++ src/main/java/fr/openent/jiraconnector/core/constants/Field.java
@@ -18,7 +18,12 @@ public class Field {
     public static final String ADDRESS = "address";
     public static final String JIRA_SSO_GROUPS = "jira-groups";
     public static final String JIRA_UAI_ADMIN = "uai-admin";
-
+    public static final String SCIM_URL = "scim-url";
+    public static final String SCIM_TOKEN = "scim-token";
+    public static final String ADMIN_JIRA_API_URL = "admin-jira-api-url";
+    public static final String ADMIN_JIRA_API_AUTHORIZATION = "admin-jira-api-authorization";
+    public static final String ADMIN_JIRA_DIRECTORY_ID = "admin-jira-directory-id";
+    
     public static final String GROUP = "group";
     public static final String LOGIN = "login";
     public static final String EMAIL = "email";
@@ -26,6 +31,8 @@ public class Field {
     public static final String STRUCTURES = "structures";
     public static final String ACADEMIES = "academies";
     public static final String USER_ID = "userId";
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
 
 
 
